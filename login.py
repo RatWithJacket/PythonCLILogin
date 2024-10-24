b9b797641f66a79e516e4df16d39ae172e889883
LiteralHat <literalhatreloaded@gmail.com>
2024-10-24 18:14:18 +1100

    finished base program

--- login.py
+++ login.py
@@ -17,6 +17,7 @@ os.system("color") # For colors in the terminal! :)
 # ANSI escape codes for cyan text and background
 CYAN = "\033[36m"      # Text color cyan
 RESET = "\033[0m"      # Reset to default
+# TODO add more colors!!!!!!!!!!
 
 # Database connection function
 def open_db():
@@ -111,7 +112,6 @@ def create_account():
                 elif length > 64:
                     print("Password too long, please specify a number below 64.")
                 else:
-                    # TODO pls put this into its own function maybe it looks awful right now sobs
                     # For now it just declares variables with the default
                     characters = string.ascii_letters
                     # Then append associated customisation variables if they are true from the earlier input
@@ -120,11 +120,21 @@ def create_account():
                     if use_symbols:
                         characters += string.punctuation
                     new_random_password = ''.join(secrets.choice(characters) for _ in range(length))
+                    print("Account created successfully! You can now log in.")
                     print("Here is your newly generated password: ")
                     print(f"{CYAN}{new_random_password}{RESET}")
                     print("Please write it down or store it somewhere safe.")
+                    # TODO pls put this into its own function maybe it looks awful right now sobs
             except ValueError:
                 print("Please enter a valid number.")
                 
+def view_account():
+    print("\n┌──────────────────┐")
+    print(" FULL LIST OF USERS ")
+    print("└──────────────────┘")
+    accounts = open_db()
+    for user, password in accounts.items():
+        print(f"{user}  |  {password}")
+
 
 display_home_menu()
