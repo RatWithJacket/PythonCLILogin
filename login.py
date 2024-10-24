7a8c2428edbd8825efd8b50f6406e36bafc84dd3
LiteralHat <literalhatreloaded@gmail.com>
2024-10-24 15:57:12 +1100

    imported some modules to make password gen a bit less ludicrious

--- login.py
+++ login.py
@@ -9,6 +9,8 @@
 # Required libraries
 import sys 
 import time 
+import secrets
+import string
 
 # Database connection function
 def open_db():
@@ -30,7 +32,7 @@ def display_home_menu():
     print("2 | CREATE ACCOUNT")
     print("3 | VIEW ACCOUNTS")
     print("4 | EXIT")
-    i = input("\nPick an option: [1/2/3/4]")
+    i = input("\nPick an option [1/2/3/4] : ")
     # Essentially this switch case statement acts like a router 
     match i:
         case "1":
@@ -61,11 +63,11 @@ def user_login():
             time.sleep(1)
             sys.exit()
         else:
-            print("\nIncorrect password. Please reenter your credentials.")
-            user_login()
+            print("\nIncorrect password. Login failed.")
+            display_home_menu()
     else:
         print("\nCredentials not found. Please double check or register.")
-        user_login()
+        display_home_menu()
 # TODO maybe split the username and password thing into its own functions so they dont have to go back??? or add sleep function so it seems slightly more interactive or whatever
 
 def create_account():
@@ -84,10 +86,29 @@ def create_account():
         print("PASSWORD CREATION MENU")
         print("1 | CREATE YOUR OWN PASSWORD")
         print("2 | RANDOMLY GENERATE PASSWORD")
-        password_gen_option = input("\nPick an option: [1/2]")
-
-
-
+        password_gen_option = input("\nPick an option [1/2] : ")
+        if password_gen_option == "1":
+            new_password = input("\nEnter a new password: ")
+            new_line = new_username + "," + new_password
+            with open("accounts.txt", "a") as db:
+                db.write("\n" + new_line)
+                db.close()
+            print("New account created successfully. You can try login now.")
+        else:
+            print("Would you like your new password to contain: ")
+            use_numbers = input("Include numbers? (y/n): ").strip().lower() == 'y'
+            use_symbols = input("Include symbols? (y/n): ").strip().lower() == 'y'
+            try:
+                length = int(input("Enter the desired password length (max 64): "))
+                if length < 0:
+                    print("Please enter a positive integer.")
+                elif length > 64:
+                    print("Password too long, please specify a number below 64.")
+                else:
+                    
+            except ValueError:
+                print("Please enter a valid number.")
+                
         
 
 display_home_menu()
